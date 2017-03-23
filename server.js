409eaca0c6e365ea13c5595cc51182f3967255df
Uzochukwu Odozi <uzo.odozi@gmail.com>
2017-03-23 17:22:15 +0200

    Added more functions to the server.js file

--- server.js
+++ server.js
@@ -12,5 +12,49 @@ module.exports = {
         return arr.reduce(function(a, b){
             return a + b
         }, 0) / (arr.length === 0 ? 1 : arr.length);
+    }, 
+
+    arrayMax: function(arr){
+    	return arr.reduce(function(a, b){
+    		return Math.max(a, b);
+    	})
+    },
+
+    arrayMin: function(arr){
+    	return arr.reduce(function(a, b){
+    		return Math.min(a, b);
+    	})
+    },
+
+    arrayUniq: function(arr){
+        var res = [];
+        for (var i = 0; i < arr.length; i++){
+            if (res.indexOf(arr[i]) == -1){
+                res.push(arr[i]);
+            }
+        }
+        return res;
+    },
+    
+    arrayUniqSort: function(arr){
+        var res = [];
+        for (var i = 0; i < arr.length; i++){
+            if (res.indexOf(arr[i]) == -1){
+                res.push(arr[i]);
+            }
+        }
+        var sort = res.sort(function(a,b){return a - b;})
+        return sort;
+    },
+    
+    arrayUniqString: function(arr){
+        var res = [];
+        for (var i = 0; i < arr.length; i++){
+            if (res.indexOf(arr[i]) == -1){
+                res.push(arr[i]);
+            }
+        }
+        
+        return res.sort();
     }
 }
